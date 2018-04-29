0fb729f72778ff83875e2de3d032b2eb56a875ef
Zack Scholl <zack.scholl@gmail.com>
2018-04-28 22:53:57 -0700

    add flags

--- cmd/syncdir/main.go
+++ cmd/syncdir/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/schollz/syncdir"
 )
 
 func main() {
+	var port, passcode string
+	flag.StringVar(&port, "port", "8045", "port for running server")
+	flag.StringVar(&passcode, "code", "123", "passcode for running server")
+	flag.Parse()
 	// go func() {
 	// 	f, _ := os.Create("cpuprofile")
 	// 	pprof.StartCPUProfile(f)
@@ -13,7 +19,7 @@ func main() {
 	// 	os.Exit(1)
 	// }()
 
-	sd, err := syncdir.New(".", "8045", "123")
+	sd, err := syncdir.New(".", port, passcode)
 	if err != nil {
 		panic(err)
 	}
